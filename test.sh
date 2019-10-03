1c9198e0a177fbf47783c2bf1c36ee46797c68bc
CCatherineeeee <48843946+CCatherineeeee@users.noreply.github.com>
2019-10-02 22:57:35 -0600

    Adding right file

---
+++ test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Authors : Yubin Go, Yifan Chen, Daniel Kim
+# Date: 9/19/2019
+#Problem 1 Code:
+#Make sure to document how you are solving each problem!
+echo "File Name: "
+read filename
+echo "Enter a regular expression: "
+read exp
+
+grep $exp $filename
+echo "List of 303 area code phone numbers:"
+egrep "^303-[0-9]{3}-[0-9]{4}$"  $filename
+echo "Number of emails:"
+grep -c "@" $filename
+grep “@geocities.com” $filename >>email_results.txt
+
